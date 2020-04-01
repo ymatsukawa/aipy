ec050ba8d4f7d8aa02299d93cc9ea76e843d2578
ymatsukawa <ymatsukawa@example.com>
2020-04-01 15:16:27 +0000

    learned many

---
+++ weather_regression/03_regression.py
@@ -0,0 +1,2 @@
+# https://towardsdatascience.com/a-beginners-guide-to-linear-regression-in-python-with-scikit-learn-83a8f7ae2b4f
+# https://towardsdatascience.com/linear-regression-using-python-b136c91bf0a2
